b4ede54a20201b64280130dbd505a340764a44c0
Nayath <aitasanz@hotmail.com>
2019-11-01 16:23:29 +0100

    Si el player toca el extintor, el extintor hace puff y el player va más rápido :D

--- Office_break/Assets/Scripts/Player.cs
+++ Office_break/Assets/Scripts/Player.cs
@@ -7,11 +7,15 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     private float _speed = 3.5f;
+    [SerializeField]
+    private float _boost = 2f;
+
+    public float currentspeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentspeed = _speed;
     }
 
     // Update is called once per frame
@@ -20,11 +24,17 @@ public class Player : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
-        transform.Translate(direction * _speed * Time.deltaTime);
+        transform.Translate(direction * currentspeed * Time.deltaTime);
     }
 
     public void Objeto(GameObject item)
     {
         Debug.Log("He pillado un objeto :D" );
-    }
+        Debug.Log(item.tag);
+
+        if (item.tag == "Extintor")
+        {
+           currentspeed = _speed + _boost;
+        }
+        }
 }
