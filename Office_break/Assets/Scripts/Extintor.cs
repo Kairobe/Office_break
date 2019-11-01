1c8e93eb3772b8e9a6177ce9af133151b1179683
Nayath <aitasanz@hotmail.com>
2019-11-01 16:37:21 +0100

    Creado Spawner que hace aparecer extintores (y lo que haga falta, pero por ahora solo extintores)

--- Office_break/Assets/Scripts/Extintor.cs
+++ Office_break/Assets/Scripts/Extintor.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 
 public class Extintor : MonoBehaviour
 {
-    [SerializeField]
-    private List<Vector3> posiciones;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        posiciones.Add( new Vector3(1, 1, 1));
-        posiciones.Add(new Vector3(0, 0, 0));
-        //Vector3 posInit = posiciones[Random.Range(0, posiciones.Length-1)];
-        Debug.Log(posiciones.ToString());
-        transform.position = posiciones[0];
+       
     }
 
     // Update is called once per frame
---
+++ Office_break/Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _extintorPrefab;
+    [SerializeField]
+    private List<Vector3> _posicionesExtintor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InicializarExtintores();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void InicializarExtintores()
+    {
+        _posicionesExtintor.Add(new Vector3(1, 1, 1));
+        _posicionesExtintor.Add(new Vector3(0, 0, 0));
+        Vector3 posInit = _posicionesExtintor[Random.Range(0, _posicionesExtintor.Count)];
+        GameObject newEnemy = Instantiate(_extintorPrefab, posInit, Quaternion.identity);
+    }
+}
