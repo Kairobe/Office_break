0ca1395393dcd698ae342a85d79a1f267e9d03c4
Carlos Zamora Zamora <carzaza@outlook.es>
2019-11-04 22:56:05 +0100

    Cambiada clase extintor para que herede de objeto.

--- Office_break/Assets/Scripts/Extintor.cs
+++ Office_break/Assets/Scripts/Extintor.cs
@@ -1,27 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Extintor : MonoBehaviour
+public class Extintor : Objeto
 {
-
-
-
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-       
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        
         if (other.tag == "Player")
         {
             Player player = other.transform.GetComponent<Player>();
