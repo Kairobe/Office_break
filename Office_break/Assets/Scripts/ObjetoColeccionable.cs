59690f49fcfa48275f14c06b4a67519c3220a723
Carlos Zamora Zamora <carzaza@outlook.es>
2019-11-04 23:21:58 +0100

    Hecha abstracta la clase ObjetoColeccionable.

--- Office_break/Assets/Scripts/ObjetoColeccionable.cs
+++ Office_break/Assets/Scripts/ObjetoColeccionable.cs
@@ -1,18 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ObjetoColeccionable : Objeto
+﻿public abstract class ObjetoColeccionable : Objeto
 {
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
