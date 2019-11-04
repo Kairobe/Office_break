7a10b479f7332706d616d5b568c5e8e199639edb
ilevelin <47749162+ilevelin@users.noreply.github.com>
2019-11-05 00:50:58 +0100

    Update Player.cs
    Script para el movimiento del jugador. Incluye ambos tipos de controles (Strafe y Tank) para poder probar ambos mas profundamente.

--- Office_break/Assets/Scripts/Player.cs
+++ Office_break/Assets/Scripts/Player.cs
@@ -11,6 +11,7 @@ public class Player : MonoBehaviour
     private float _boost = 2f;
 
     public float currentspeed;
+    public bool tankControls;
 
     // Start is called before the first frame update
     void Start()
@@ -23,8 +24,16 @@ public class Player : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
-        transform.Translate(direction * currentspeed * Time.deltaTime);
+        if(tankControls){
+            Vector3 direction = new Vector3(0, 0, verticalInput);
+            Vector3 rotation = new Vector3(0, horizontalInput, 0);
+            transform.Translate(direction * currentspeed * Time.deltaTime);
+            transform.Rotate(rotation * currentspeed * 0.25f);
+            if(gameObject.transform.rotation.y > 180) gameObject.transform.rotation.SetAxisAngle(new Vector3(0, 1, 0), 180);
+        }else{
+            Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+            transform.Translate(direction * currentspeed * Time.deltaTime);
+        }
     }
 
     public void Objeto(GameObject item)
