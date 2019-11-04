3143ad4a3ec91b39d85c1ac3d878d7b88f3982c3
Zaka <zabou@inf.upv.es>
2019-11-04 22:22:16 +0100

    Objetos
    Añadida la clase objeto, maletin y clip. Éstas dos últimas heredan de la primera

---
+++ Office_break/Assets/Scripts/Objeto.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Objeto : MonoBehaviour
+{
+    public List<Vector3> availablePositions = new List<Vector3>();
+    public Vector3 objectPosition = new Vector3();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public abstract void consumePosition();
+}
