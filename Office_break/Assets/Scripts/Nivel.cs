4a19b8ef9fe75ccf66e61bbf23161a22dd81c43f
Carlos Zamora Zamora <carzaza@outlook.es>
2019-11-05 00:18:04 +0100

    Implementado script para generar objetos en el nivel.

--- Office_break/Assets/Scripts/Nivel.cs
+++ Office_break/Assets/Scripts/Nivel.cs
@@ -1,21 +1,73 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Nivel : MonoBehaviour
 {
+    private Dictionary<string, GameObject> gameObjectPrefabs;
+
+    private Dictionary<string, int> maxNumberOfObjectByType, collectedObjectNumber;
+
+    [SerializeField]
+    private GameObject clipPrefab, maletinPrefab;
+
+    [SerializeField]
+    private int clipMaxNumber, maletinMaxNumber = 0;
+
     public List<Vector3> availablePositions = new List<Vector3>();
-    public List<Object> availableObjects = new List<Object>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        availablePositions.Add(new Vector3(1, 1, 0));
+        availablePositions.Add(new Vector3(2, 1, 0));
+        availablePositions.Add(new Vector3(3, 1, 0));
+        availablePositions.Add(new Vector3(4, 1, 0));
+        availablePositions.Add(new Vector3(5, 1, 0));
+
+        this.collectedObjectNumber = new Dictionary<string, int>();
+        this.collectedObjectNumber.Add(nameof(Clip), 0);
+        this.collectedObjectNumber.Add(nameof(Maletin), 0);
+
+        this.gameObjectPrefabs = new Dictionary<string, GameObject>();
+        this.gameObjectPrefabs.Add(nameof(Clip), clipPrefab);
+        this.gameObjectPrefabs.Add(nameof(Maletin), maletinPrefab);
+
+        this.maxNumberOfObjectByType = new Dictionary<string, int>();
+        this.maxNumberOfObjectByType.Add(nameof(Clip), clipMaxNumber);
+        this.maxNumberOfObjectByType.Add(nameof(Maletin), maletinMaxNumber);
+
+        InitializeObjects();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    /// <summary> Increases the number of collected objects of the given type in the given units. </summary>
+    /// <param name="objectType"> The type of the object to increase the collected number. </param>
+    /// <param name="units">
+    /// (Optional) The total ammount of units to increase. By default it is set to one unit.
+    /// </param>
+    public void IncreaseCollectedNumber(string objectType, int? units = null)
+    {
+        int unitsToIncrease = units ?? 1;
+
+        this.collectedObjectNumber[objectType] += unitsToIncrease;
+    }
+
+    /// <summary> Initializes the different objects in random positions in the scene. </summary>
+    private void InitializeObjects()
+    {
+        foreach (string objectType in this.maxNumberOfObjectByType.Keys)
+        {
+            for (int i = 0; i < this.maxNumberOfObjectByType[objectType]; i++)
+            {
+                Vector3 position = availablePositions[Random.Range(0, availablePositions.Count)];
+                Instantiate(this.gameObjectPrefabs[objectType], position, Quaternion.identity);
+
+                availablePositions.Remove(position);
+            }
+        }
     }
 }
--- Office_break/Assets/Scripts/ObjetoColeccionable.cs
+++ Office_break/Assets/Scripts/ObjetoColeccionable.cs
@@ -1,5 +1,9 @@
-﻿public abstract class ObjetoColeccionable : Objeto
+﻿using UnityEngine;
+
+public abstract class ObjetoColeccionable : Objeto
 {
+    private Nivel nivel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -8,5 +12,21 @@
     // Update is called once per frame
     void Update()
     {
+    }
+
+    private void Awake()
+    {
+        nivel = GameObject.Find("Nivel").GetComponent<Nivel>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            this.nivel.IncreaseCollectedNumber(this.tag);
+
+            //Eliminar extintor
+            Destroy(this.gameObject);
+        }
     }
 }
