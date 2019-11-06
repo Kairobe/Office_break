9f4bfabbedd23bd37f2df3c6550b1cbcd05e9304
Carlos Zamora Zamora <carzaza@outlook.es>
2019-11-06 17:01:45 +0100

    Refactorizada la inicialización del nivel.

--- Office_break/Assets/Scripts/Nivel.cs
+++ Office_break/Assets/Scripts/Nivel.cs
@@ -13,44 +13,86 @@ public class Nivel : MonoBehaviour
     [SerializeField]
     private int clipMaxNumber, maletinMaxNumber, extintorMaxNumber = 0;
 
-    public List<Vector3> availablePositions = new List<Vector3>();
+    public List<(Vector3 position, int rotation)> availablePositions;
 
     private ControladorUi controladorUi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializePositionsAndRotation();
+
+        InitializeObjectsToCollect();
+
+        InitializeMaximunNumberOfInstancesByGameObject();
+
+        InitializeGameObjectPrefabs();
+
+        InitializeObjects();
+    }
 
     private void Awake()
     {
         this.controladorUi = GameObject.FindGameObjectWithTag("PropiedadesUi").GetComponent<ControladorUi>();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        availablePositions.Add(new Vector3(1, 3, 0));
-        availablePositions.Add(new Vector3(2, 3, 0));
-        availablePositions.Add(new Vector3(3, 3, 0));
-        availablePositions.Add(new Vector3(4, 3, 0));
-        availablePositions.Add(new Vector3(5, 3, 0));
-
-        this.collectedObjectNumber = new Dictionary<string, int>();
-        this.collectedObjectNumber.Add(nameof(Clip), 0);
-        this.collectedObjectNumber.Add(nameof(Maletin), 0);
-
-        this.gameObjectPrefabs = new Dictionary<string, GameObject>();
-        this.gameObjectPrefabs.Add(nameof(Clip), clipPrefab);
-        this.gameObjectPrefabs.Add(nameof(Maletin), maletinPrefab);
-        this.gameObjectPrefabs.Add(nameof(Extintor), extintorPrefab);
-
-        this.maxNumberOfObjectByType = new Dictionary<string, int>();
-        this.maxNumberOfObjectByType.Add(nameof(Clip), clipMaxNumber);
-        this.maxNumberOfObjectByType.Add(nameof(Maletin), maletinMaxNumber);
-        this.maxNumberOfObjectByType.Add(nameof(Extintor), extintorMaxNumber);
-
-        InitializeObjects();
-    }
-
     // Update is called once per frame
     void Update()
     {
+    }
+
+    /// <summary>
+    /// Initializes the manixum number of instances that must be generated of each Game Object type.
+    /// </summary>
+    private void InitializeMaximunNumberOfInstancesByGameObject()
+    {
+        this.maxNumberOfObjectByType = new Dictionary<string, int>
+        {
+            { nameof(Clip), clipMaxNumber },
+            { nameof(Maletin), maletinMaxNumber },
+            { nameof(Extintor), extintorMaxNumber }
+        };
+    }
+
+    /// <summary> Initializes the prefabs of the different Game Objects to be created. </summary>
+    private void InitializeGameObjectPrefabs()
+    {
+        this.gameObjectPrefabs = new Dictionary<string, GameObject>
+        {
+            { nameof(Clip), clipPrefab },
+            { nameof(Maletin), maletinPrefab },
+            { nameof(Extintor), extintorPrefab }
+        };
+    }
+
+    /// <summary> Initializes the Game Objects that are collectable. </summary>
+    private void InitializeObjectsToCollect()
+    {
+        this.collectedObjectNumber = new Dictionary<string, int>
+        {
+            { nameof(Clip), 0 },
+            { nameof(Maletin), 0 }
+        };
+    }
+
+    /// <summary>
+    /// Initializes the available positions and the rotation that must be applied to the created
+    /// Game Object.
+    /// </summary>
+    private void InitializePositionsAndRotation()
+    {
+        availablePositions = new List<(Vector3 position, int rotation)>();
+
+        availablePositions.Add((new Vector3(17.5f, 0.5f, -20), 0));
+        availablePositions.Add((new Vector3(17.5f, 0.5f, -28), 0));
+        availablePositions.Add((new Vector3(16, 0.5f, -32.5f), 90));
+        availablePositions.Add((new Vector3(10f, 0.5f, -32.5f), 90));
+        availablePositions.Add((new Vector3(1, 0.5f, -33), 0));
+        availablePositions.Add((new Vector3(1, 0.5f, -24.5f), 0));
+        availablePositions.Add((new Vector3(-1.5f, 0.5f, -15), 0));
+        availablePositions.Add((new Vector3(5.3f, 0.5f, -11.5f), 90));
+        availablePositions.Add((new Vector3(6, 0.5f, -15.8f), 90));
+        availablePositions.Add((new Vector3(12, 0.5f, -17), 90));
     }
 
     /// <summary> Increases the number of collected objects of the given type in the given units. </summary>
@@ -76,10 +118,12 @@ public class Nivel : MonoBehaviour
 
             for (int i = 0; i < this.maxNumberOfObjectByType[objectType]; i++)
             {
-                Vector3 position = availablePositions[Random.Range(0, availablePositions.Count)];
-                Instantiate(prefab, position, prefab.transform.rotation);
+                (Vector3 position, int rotation) = availablePositions[Random.Range(0, availablePositions.Count)];
 
-                availablePositions.Remove(position);
+                GameObject createdGameObject = Instantiate(prefab, position, prefab.transform.rotation);
+                createdGameObject.transform.Rotate(new Vector3(0, 0, rotation));
+
+                availablePositions.Remove((position, rotation));
             }
         }
     }
