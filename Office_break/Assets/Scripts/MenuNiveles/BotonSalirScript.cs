a19b8327f21573acd1864b06c106f74616c2bbca
Nayath <aitasanz@hotmail.com>
2019-11-21 15:53:14 +0100

    Añadido menú inicial

---
+++ Office_break/Assets/Scripts/MenuNiveles/BotonSalirScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseRandomColor : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(ChangeImageColor);
+    }
+
+    private void ChangeImageColor()
+    {
+        GetComponent<Image>().color = RandomColor();
+    }
+
+    private Color RandomColor()
+    {
+        return new Color(
+            UnityEngine.Random.Range(0f, 1f),
+            UnityEngine.Random.Range(0f, 1f),
+            UnityEngine.Random.Range(0f, 1f)
+            );
+    }
+}
---
+++ Office_break/Assets/Scripts/MenuNiveles/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Quit()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void ShowScene(int level)
+    {
+        /*if (currentMenu != null)
+            currentMenu.IsOpen = false;
+        currentMenu = null;*/
+        Application.LoadLevel(level);
+    }
+}
