8907126019e7e2337af227db7fbb4845f39d8bc6
ilevelin <47749162+ilevelin@users.noreply.github.com>
2019-11-23 16:19:03 +0100

    Nuevo script de cámara.

---
+++ Office_break/Assets/Scripts/CameraCollision2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision2 : MonoBehaviour
+{
+    [SerializeField] private float errorCorrection = 0.9f;
+    private Vector3 minPos, maxPos, posDif, futurePos;
+
+    void Start() {
+    }
+    void Update() {
+
+        minPos = transform.parent.parent.position;
+        maxPos = transform.parent.position;
+        posDif = new Vector3(
+            maxPos.x - minPos.x,
+            maxPos.y - minPos.y,
+            maxPos.z - minPos.z
+        );
+
+        RaycastHit hit;
+
+        if (Physics.Linecast(maxPos, minPos, out hit)){
+            futurePos = maxPos - new Vector3(
+                posDif.x * (hit.distance/2 + .1f),
+                posDif.y * (hit.distance/2 + .1f),
+                posDif.z * (hit.distance/2 + .1f)
+            );
+        } else
+            futurePos = maxPos - (posDif*.1f);
+
+        transform.position = futurePos;
+
+        //transform.position = Vector3.Lerp(transform.position, futurePos, Time.deltaTime * smooth);
+    }
+}
