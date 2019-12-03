d1b5bc1671aff584e3b41774390ca86248178466
Nayath <aitasanz@hotmail.com>
2019-12-03 18:14:05 +0100

    Mejorada fin de nivel

--- Office_break/Assets/Scripts/FinDeNivel.cs
+++ Office_break/Assets/Scripts/FinDeNivel.cs
@@ -7,13 +7,13 @@ using UnityEngine.UI;
 public class FinDeNivel : MonoBehaviour
 {
     private string textoEscrito;
-    private Text texto;
+     Text texto;
     // Start is called before the first frame update
     void Start()
     {
         //LevelData levelData = new LevelData("admin", 23, 34);
         //DataManager.SaveData(levelData);
-
+        texto = GetComponent<Text>();
 
         setTexto();
 
@@ -24,11 +24,11 @@ public class FinDeNivel : MonoBehaviour
     private void setTexto()
     {
         LevelData datos = DataManager.LoadData("admin");
-        Debug.Log(datos.playerAlias);
+       // Debug.Log(datos.playerAlias);
         textoEscrito = "Enhorabuena " + datos.playerAlias + "!\n \n";
         textoEscrito += "Has conseguido " + datos.collectedBriefcases + " maletines!\n \n";
         textoEscrito += "Tambien has conseguido " + datos.collectedClips + " clips!\n \n";
-        texto = GetComponent<Text>();
+       
         texto.text = textoEscrito;
     }
     void Update()
@@ -47,6 +47,25 @@ public class FinDeNivel : MonoBehaviour
             currentMenu.IsOpen = false;
         currentMenu = null;*/
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void funcionalidadPorHacer()
+    {
+        LevelData datos = DataManager.LoadData("admin");
+        //Debug.Log(datos.playerAlias);
+        textoEscrito = "Lo sentimos, esta funcionalidad aun no esta implementada :(";
+
+        //texto.text = textoEscrito;
+        texto.GetComponent<UnityEngine.UI.Text>().text = textoEscrito;
+        StartCoroutine("MantenerMensaje");
+    }
+
+    private IEnumerator MantenerMensaje()
+    {
+        
+
+        yield return new WaitForSeconds(2);
+        setTexto();
     }
 
 }
