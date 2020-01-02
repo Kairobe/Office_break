e1c7ce8f3a552518e556bdca7a09c6c84eead155
ilevelin <47749162+ilevelin@users.noreply.github.com>
2020-01-02 19:28:40 +0100

    Creado Minimapa

---
+++ Office_break/Assets/Scripts/UI/MiniMapUI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapUI : MonoBehaviour {    
+    public Transform mapMin, mapMax, player; //MIN = Arriba-Izquierda //MAX = Abajo-Derecha 
+    public float x, y;
+    private RectTransform marker;
+    void Start() {
+        marker = GetComponent<RectTransform>();
+    }
+
+    void LateUpdate() {
+        x = ((player.position.x - mapMin.position.x) / (mapMax.position.x - mapMin.position.x))*200;
+        y = -((player.position.z - mapMin.position.z) / (mapMax.position.z - mapMin.position.z))*200;
+        marker.localPosition = new Vector3(x, y, 0);
+    }
+}
