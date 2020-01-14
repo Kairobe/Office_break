ed48e52ddf3f885cf9c041363ac3be7fc34f7f38
Carlos Zamora Zamora <carzaza@outlook.es>
2020-01-14 08:34:49 +0100

    Hecha escena de inventario.

--- Office_break/Assets/Scripts/MenuNiveles/MenuManager.cs
+++ Office_break/Assets/Scripts/MenuNiveles/MenuManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
@@ -8,25 +6,22 @@ public class MenuManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
+
     public void Quit()
     {
         Application.Quit();
+
         //UnityEditor.EditorApplication.isPlaying = false;
     }
 
     public void ShowScene(string sceneName)
     {
-        /*if (currentMenu != null)
-            currentMenu.IsOpen = false;
-        currentMenu = null;*/
         SceneManager.LoadScene(sceneName);
     }
 }
---
+++ Office_break/Assets/Scripts/UI/Inventory/InventoryWindowManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InventoryWindowManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainMenuButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void NavigateToMainMenu()
+    {
+        SceneManager.LoadScene("MenuInicial");
+    }
+}
