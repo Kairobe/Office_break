19ca73c3e13e99250ffa8ba7e20dd6a0aa6c524d
Zaka <zabou@inf.upv.es>
2020-01-15 20:35:39 +0100

    añadido script de menu

---
+++ Office_break/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private Canvas canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        canvas = this.GetComponent<Canvas>();
+        canvas.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            canvas.enabled = !canvas.enabled;
+            Time.timeScale = canvas.enabled ? 0 : 1;
+        }
+    }
+
+    public void continueLevel()
+    {
+        canvas.enabled = !canvas.enabled;
+        Time.timeScale = canvas.enabled ? 0 : 1;
+    }
+
+    public void exit()
+    {
+        SceneManager.LoadScene("MenuInicial");
+    }
+
+    public void volumeMute()
+    {
+
+    }
+
+    public void volumeReduce()
+    {
+
+    }
+    
+    public void volumeIncrease()
+    {
+
+    }
+}
