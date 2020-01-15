ce3e28f1447e6ed05eda1132fb3dfbd7a6ea28ac
Zaka <zabou@inf.upv.es>
2020-01-15 21:39:44 +0100

    menu pausa casi acabado

--- Office_break/Assets/Scripts/PauseMenu.cs
+++ Office_break/Assets/Scripts/PauseMenu.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
     private Canvas canvas;
+    private Text volumeValue;
 
     // Start is called before the first frame update
     void Start()
     { 
         canvas = this.GetComponent<Canvas>();
         canvas.enabled = false;
+        volumeValue = GameObject.FindGameObjectWithTag("TextoVolumen").GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -24,29 +27,32 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-    public void continueLevel()
+    public void ContinueLevel()
     {
         canvas.enabled = !canvas.enabled;
         Time.timeScale = canvas.enabled ? 0 : 1;
     }
 
-    public void exit()
+    public void Exit()
     {
         SceneManager.LoadScene("MenuInicial");
     }
 
-    public void volumeMute()
+    public void VolumeMute()
     {
-
+        AudioListener.volume = 0;
+        volumeValue.text = AudioListener.volume * 100 + "";
     }
 
-    public void volumeReduce()
+    public void VolumeReduce()
     {
-
+        AudioListener.volume -= 1;
+        volumeValue.text = AudioListener.volume * 100 + "";
     }
     
-    public void volumeIncrease()
+    public void VolumeIncrease()
     {
-
+        AudioListener.volume += 1;
+        volumeValue.text = AudioListener.volume * 100 + "";
     }
 }
